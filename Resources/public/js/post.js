418b55be2a149e06275063674bf7dc532a6b8543
Bartosz Malec <b.malec@awaresoft.pl>
2016-08-28 00:01:33 +0200

    Moved dedicated js to application package

--- Resources/public/js/post.js
+++ Resources/public/js/post.js
@@ -40,63 +40,11 @@ var Post = Post || {
                 }
             });
         },
-        runGallery: function () {
-            $(".grid-post.grid-justify").justifiedGallery({
-                thumbnailPath: function (currentPath, width, height) {
-                    if (height < 200) {
-                        return currentPath;
-                    } else {
-                        if (width <= 250) {
-                            return currentPath.replace("_normal", "_small");
-                        } else if (width > 250 && width <= 500) {
-                            return currentPath.replace("_normal", "_500");
-                        } else if (width > 500 && width <= 720) {
-                            return currentPath.replace("_normal", "_720");
-                        } else if (width > 720 && width <= 850) {
-                            return currentPath.replace("_normal", "_850");
-                        } else {
-                            return currentPath.replace("_normal", "_big");
-                        }
-                    }
-                },
-                selector: '.item',
-                rowHeight: 200,
-                maxRowHeight: 400,
-                captions: false,
-                margins: 5,
-                lastRow: 'nojustify'
-            }).on('jg.complete', function () {
-                Post.runLightGalleryPlugin($(this));
-            });
-        },
-        runLightGalleryPlugin: function ($element) {
-            var staticOptions = {
-                selector: '.open',
-                download: false,
-                mode: 'lg-slide',
-                thumbnail: false,
-            };
-
-            $element.on('onAfterOpen.lg', function () {
-                prepareGalleryView();
-            });
-
-            $element.on('onBeforeSlide.lg', function () {
-                prepareGalleryView();
-            });
-
-            function prepareGalleryView() {
-                $('.lg-outer .lg-object').addClass('visible');
-            }
-
-            $element.lightGallery(staticOptions);
-        },
         onReady: function (Main) {
             this.main = Main;
             this.showMore();
         },
         onLoad: function (Main) {
             this.main = Main;
-            this.runGallery();
         }
     };
